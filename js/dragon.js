f8a53800fce2fab15c34bc80df0754c2c266759d
Marie880409 <153714131+Marie880409@users.noreply.github.com>
2024-06-27 14:09:37 +0900

    df
    dffdf

---
+++ js/dragon.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+      $(".dragon_eye").animate({
+        top: "431px"
+      },3000,"swing");
+    });
+
+    $(document).ready(function(){
+        $(".slide_tile").animate({
+            marginLeft: "216px",
+            opacity:"0.8"
+        },2000,"swing");
+    });
+
+    $(document).ready(function(){
+        $(".slide_tile_02").animate({
+            marginTop: "109px"
+        },2000,"swing");
+    });
---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination-bullet',
+    type:"bullets",
+    clickable : true
+  }
+
+});
+
